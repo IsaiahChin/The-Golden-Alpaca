299feed1f2bbb770a9c4b53efaf3b528e60c4fe5
isaiah497 <68924638+isaiah497@users.noreply.github.com>
2020-09-16 20:29:09 +1200

    Renamed scripts

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,111 +1,36 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.UIElements;
-using Image = UnityEngine.UI.Image;
 
-public class PlayerHealth : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    [Header("Health Stats")]
-    public float maxHealth;
-    public float currentHealth;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+    public Sprite deadSprite;
+    public float health;
 
-    [Header("Heart Assets")]
-    public GameObject canvas;
-    public GameObject heartStorage;
-    [Space]
-    public GameObject emptyHeart;
-    public GameObject halfHeart;
-    public GameObject fullHeart;
-    public GameObject lastHalfHeart;
-
-    [Header("Game Over Assets")]
-    public GameObject gameOverText;
-
-    private Status status;
-
-    private readonly float healthIncrement = 0.5f;
+    private GameObject heartStorage;
+    private PlayerHealth playerHealthScript;
 
     // Start is called before the first frame update
     void Start()
     {
-        maxHealth = 3;
-        currentHealth = maxHealth;
-        UpdateHealth();
-        status = canvas.GetComponent<Status>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+
+        heartStorage = GameObject.Find("Heart Storage");
+        playerHealthScript = heartStorage.GetComponent<PlayerHealth>();
+        health = playerHealthScript.maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow)) //Increase health by one half
+        health = playerHealthScript.currentHealth;
+        if (health <= 0.0f)
         {
-            currentHealth += healthIncrement;
-            UpdateHealth();
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow)) //Decrease health by one half
-        {
-            currentHealth -= healthIncrement;
-            UpdateHealth();
-        }
-    }
-
-    private void UpdateHealth()
-    {
-        if (currentHealth > maxHealth) //Check if health goes over max health
-        {
-            currentHealth = maxHealth; //Don't allow health overflow
-        }
-        else
-        {
-            foreach (Transform heart in transform) //Remove all heart prefabs from the Heart Storage
-            {
-                Destroy(heart.gameObject);
-            }
-
-            for (int i = 0; i < maxHealth; i++) //Instantiate heart prefabs
-            {
-                if (currentHealth == i + 1)
-                {
-                    Instantiate(fullHeart, heartStorage.transform);
-                }
-                else if (currentHealth > i)
-                {
-                    if (currentHealth < (i + 1))
-                    {
-                        if (currentHealth == 0.5f)
-                        {
-                            Instantiate(lastHalfHeart, heartStorage.transform);
-                        }
-                        else
-                        {
-                            Instantiate(halfHeart, heartStorage.transform);
-                        }
-                    }
-                    else
-                    {
-                        Instantiate(fullHeart, heartStorage.transform);
-                    }
-                }
-                else
-                {
-                    Instantiate(emptyHeart, heartStorage.transform);
-                }
-            }
-
-            if (currentHealth <= 0.0f) //Check if player is dead
-            {
-                currentHealth = 0.0f;
-                if (status != null)
-                {
-                    status.UpdateText("Dead"); //Update health status text
-                }
-                //Create Game Over Text in parent object (Script is attached to Heart Storage object, child of Canvas)
-                Instantiate(gameOverText, transform.parent.gameObject.transform);
-                this.enabled = false; //Disable this script
-            }
+            animator.enabled = false;
+            spriteRenderer.sprite = deadSprite;
         }
     }
 }
