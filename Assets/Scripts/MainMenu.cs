f50d76e5ee106af033c736b8929d9cc9b5ac2ca3
jnnfr-koo <60792959+jnnfr-koo@users.noreply.github.com>
2020-09-16 22:37:21 +1200

    Main Menu w/ script

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //Start the game. Uses unity build to go to starting level
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+    //Exit the game
+    public void QuitGame()
+    {
+        Debug.Log("Game Quit");
+        Application.Quit();
+    }
+}
