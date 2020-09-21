312b800b16ccf429b80b6b045837ed78626c92af
jnnfr-koo <60792959+jnnfr-koo@users.noreply.github.com>
2020-09-21 20:21:14 +1200

    Options Button Function working

---
+++ Assets/Scripts/BgScroll.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class BgScroll : MonoBehaviour
+{
+    //code for infinite scrolling background
+
+    public float speed = 5f;
+    public float clamp;
+    public Vector3 startPos;  
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+      
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        //Move image for scrolling background effect
+        float newPosition = Mathf.Repeat(Time.time * speed, clamp);
+        transform.position = startPos + Vector3.left * newPosition;
+    }
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MainMenu : MonoBehaviour
 {
     //Start the game. Uses unity build to go to starting level
