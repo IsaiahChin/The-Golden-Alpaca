e46501d9e5bbf85ba382beb566a0afa50fbcd3cf
darion <nft4407@autuni.ac.nz>
2020-09-23 14:19:37 +1200

    Created Movement Tests
    Removed movement code to purpously fail.

--- Assets/Tests/PlayerMovementTests.cs
+++ Assets/Tests/PlayerMovementTests.cs
@@ -18,5 +18,29 @@ namespace Tests
 
             Assert.AreEqual(new Vector3(5, 0, 0), movementTest.CalculateMovement(xAxisMovement));
         }
+
+        [Test]
+        public void Movement_On_Z_Axis_Works()
+        {
+            int speed = 5;
+            Vector3 xAxisMovement = new Vector3(0, 0, 1);
+
+            PlayerMovement movementTest = new PlayerMovement(speed);
+
+            Assert.AreEqual(new Vector3(0, 0, 5), movementTest.CalculateMovement(xAxisMovement));
+        }
+
+        [Test]
+        public void Movement_On_Daigonals_Work()
+        {
+            int speed = 5;
+            Vector3 xAxisMovement = new Vector3(1, 0, 1);
+
+            PlayerMovement movementTest = new PlayerMovement(speed);
+
+            float normalizedSpeed = 5.0f / Mathf.Sqrt(50.0f);
+
+            Assert.AreEqual(new Vector3(normalizedSpeed, 0, normalizedSpeed), movementTest.CalculateMovement(xAxisMovement));
+        }
     }
 }
