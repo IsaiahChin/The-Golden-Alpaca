60e9f7cd26abe58d32544f01ace570fe1ab73517
darion <nft4407@autuni.ac.nz>
2020-09-23 13:49:01 +1200

    Made OutdatedPlayer
    Being used to test movement.

--- Assets/Scripts/Outdated/OutdatedPlayer.cs
+++ Assets/Scripts/Outdated/OutdatedPlayer.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class OutdatedPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Components
+    private Rigidbody rigidBody;
+    private Animator animator;
+    private Vector3 newPosition;
+
+    // Scripts
+    private PlayerMovement movement;            // Holds new input direction from user to move to
+    private PlayerHealthUI playerHealthScript;  // Links health UI to player
+
+    // Player attributes
+    public float health { get; set; }
+    public float maxHealth { get; set; }
+    [SerializeField]
+    private float maxSpeed;
+
+    private void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
+        movement = new PlayerMovement(maxSpeed);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Get player input for new direction, and store it
+        newPosition = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        // Call method to add new vector to the speed of the player
+        rigidBody.velocity = movement.CalculateMovement(newPosition);
     }
 }
