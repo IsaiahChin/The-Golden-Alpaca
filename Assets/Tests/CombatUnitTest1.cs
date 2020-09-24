c8cb1bd49f85066233c9627460436111374c5ee6
MatthewCCopeland <69947789+MatthewCCopeland@users.noreply.github.com>
2020-09-25 00:58:20 +1200

    Combat Test work

---
+++ Assets/Tests/CombatUnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class CombatUnitTest1
+    {
+        
+        // A Test behaves as an ordinary method
+        [Test]
+        public void InstantiateBulletObject()
+        {
+           
+
+            //GameObject bullet = MonoBehaviour.Instantiate(Resources.Load<GameObject>("BulletTest"));
+            //Assert.IsNotNull(bullet, "Bullet Instantialized");
+            // Use the Assert class to test conditions
+        }
+
+    }
+}
