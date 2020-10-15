2d83264f6a4b68a39e4777d287643b01f04e4a2f
MatthewCCopeland <69947789+MatthewCCopeland@users.noreply.github.com>
2020-10-15 17:49:55 +1300

    Initial Navmesh Work

--- Assets/Scripts/NavmeshBaker.cs
+++ Assets/Scripts/NavmeshBaker.cs
@@ -1,14 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.AI;
 using UnityEngine.AI;
 
 public class NavmeshBaker : MonoBehaviour
 {
+    
+
     //public NavMeshSurface
     void Start()
     {
-        UnityEditor.AI.NavMeshBuilder.BuildNavMesh();
+       
     }
 }
