4cd11c51eec382cafcb5d230a32e690c750c78a9
darion <nft4407@autuni.ac.nz>
2020-10-16 16:50:19 +1300

    Created Door Opening Code and TestPrefab

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    [SerializeField]
+    private Sprite openedDoor;
+
+    private void Awake()
+    {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    public void OpenDoor()
+    {
+        sr.sprite = openedDoor;
+    }
+}
