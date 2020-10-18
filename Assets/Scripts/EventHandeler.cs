cd59d0a77cd5d00f14e62ae5026c7c4227346ba9
darion <nft4407@autuni.ac.nz>
2020-10-19 12:38:20 +1300

    Creating Player Spawn Event

--- Assets/Scripts/EventHandeler.cs
+++ Assets/Scripts/EventHandeler.cs
@@ -7,8 +7,16 @@ public class EventHandeler : MonoBehaviour
     public delegate void DoorInteraction();
     public static event DoorInteraction OnDoorInteraction;
 
+    public delegate void PlayerSpawn();
+    public static event PlayerSpawn OnPlayerSpawn;
+
     public static void ActivateInteraction()
     {
         OnDoorInteraction();
+    }
+
+    public static void ActivatePlayerSpawn()
+    {
+        OnPlayerSpawn();
     }
 }
