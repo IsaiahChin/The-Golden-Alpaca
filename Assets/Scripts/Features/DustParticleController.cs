8ef5ea83a8c8d2d77bf46a264210a84b1f70cd65
isaiah497 <68924638+isaiah497@users.noreply.github.com>
2020-10-18 16:15:12 +1300

    Added some particle effects
    - Alpaca sparkles
    - Projectiles leave a trail of sparkles
    - Dust particles appear when moving

---
+++ Assets/Scripts/Features/DustParticleController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DustParticleController : MonoBehaviour
+{
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.Find("Alpaca");
+    }
+
+    void Update()
+    {
+        foreach (Transform particleEffect in transform)
+        {
+            ParticleSystem particleSystem = particleEffect.GetComponent<ParticleSystem>();
+            ParticleSystem.EmissionModule emitter = particleSystem.emission;
+            emitter.enabled = (player.GetComponent<Rigidbody>().velocity != Vector3.zero);
+        }
+    }
+}
