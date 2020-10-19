c14cb85d160cce31d024938bca1dd9ed2bba4dcc
isaiah497 <68924638+isaiah497@users.noreply.github.com>
2020-10-19 22:04:04 +1300

    Finished particle effect feature
    - Puff of smoke now appears when enemies die
    - Added comments to code

--- Assets/Scripts/Enemy/EnemyHealthController.cs
+++ Assets/Scripts/Enemy/EnemyHealthController.cs
@@ -7,6 +7,9 @@ public class EnemyHealthController : MonoBehaviour
     public float health;
     public Animator animator;
 
+    [SerializeField]
+    private GameObject deathCloudObject;
+
     private void Update()
     {
         if (health<=0)
@@ -17,6 +20,7 @@ public class EnemyHealthController : MonoBehaviour
 
     private void Die()
     {
+        Instantiate(deathCloudObject, transform.position, new Quaternion(0, 0, 0, 0));
         Destroy(gameObject);
     }
 
--- Assets/Scripts/Features/DustParticleController.cs
+++ Assets/Scripts/Features/DustParticleController.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+/**
+ * This class enables all particle effects in a given object when the player is moving
+ */
 
 public class DustParticleController : MonoBehaviour
-{
+{   
     private GameObject player;
 
     void Start()
@@ -13,10 +16,14 @@ public class DustParticleController : MonoBehaviour
 
     void Update()
     {
+        //Loop through each particle system
         foreach (Transform particleEffect in transform)
         {
+            //Get reference to the particle system and emission module 
             ParticleSystem particleSystem = particleEffect.GetComponent<ParticleSystem>();
             ParticleSystem.EmissionModule emitter = particleSystem.emission;
+            
+            //Enable emission module if the player is moving
             emitter.enabled = (player.GetComponent<Rigidbody>().velocity != Vector3.zero);
         }
     }
