f7e1c47d9ea8eb3bc655d471eaf7449cf3903d72
darion <nft4407@autuni.ac.nz>
2020-10-20 19:25:45 +1300

    Ajusting Mediator to Allow for More Mediation

--- Assets/Scripts/UIMediator.cs
+++ Assets/Scripts/UIMediator.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class UIMediator : MonoBehaviour
 {
-    private bool mediationAccomplished = false;
+    private bool uiMediationAccomplished = false;
+    private bool exitMediationAccomplished = false;
 
     void Update()
+    {
+        UIMediation();
+
+        if (uiMediationAccomplished && exitMediationAccomplished)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void UIMediation()
     {
         PlayerHealthUI_Refactor healthInfo = GameObject.Find("HealthCanvas").GetComponentInChildren<PlayerHealthUI_Refactor>();
         GameObject player = GameObject.Find("Alpaca");
@@ -18,13 +29,8 @@ public class UIMediator : MonoBehaviour
             if (healthStartHasRun)
             {
                 EventHandeler.ActivateHealthUI();
-                mediationAccomplished = true;
+                uiMediationAccomplished = true;
             }
-        }
-
-        if (mediationAccomplished)
-        {
-            Destroy(gameObject);
         }
     }
 }
